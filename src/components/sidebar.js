05daa0b9e6bc7d92d34fa05f4b89b003314740eb
Egor <zinchenko02051999@gmail.com>
2024-03-06 18:40:41 +0300

    started work on appearing description in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,20 +1,24 @@
 
 import '../styles/Sidebar.css'
-import {useState} from "react";
 
 
 function Sidebar () {
 
-    const [sidebarExtension, setSidebarExtension] = useState(false);
 
     function moreBtnClicked() {
-        setSidebarExtension(!sidebarExtension)
-        sidebarExtension ? document.querySelector('.sidebar').classList.add('sidebar-extended') : document.querySelector('.sidebar').classList.remove('sidebar-extended')
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.classList.toggle('sidebar-extended')
     }
 
     return (
         <aside className={'sidebar'}>
             <div className={'sidebar-btn sidebar-btn-more btn'} onClick={moreBtnClicked}>more</div>
+            <div className={'sidebar-filters'}>
+                <div className="sidebar-filter">O</div>
+                <div className="sidebar-filter">O</div>
+                <div className="sidebar-filter">O</div>
+                <div className="sidebar-filter">O</div>
+            </div>
         </aside>
     )
 }
