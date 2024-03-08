1327a333f2084b3244fe5381599ebba27feb06f8
Egor <zinchenko02051999@gmail.com>
2024-03-08 10:35:10 +0300

    updated sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,17 +7,33 @@ function Sidebar () {
 
     function moreBtnClicked() {
         const sidebar = document.querySelector('.sidebar')
+        const sidebar_filter_texts = document.querySelectorAll('.sidebar-filter-text')
         sidebar.classList.toggle('sidebar-extended')
+        sidebar_filter_texts.forEach((text) => {
+            text.classList.toggle('sidebar-filter-text-visible')
+        })
     }
 
     return (
         <aside className={'sidebar'}>
             <div className={'sidebar-btn sidebar-btn-more btn'} onClick={moreBtnClicked}>more</div>
             <div className={'sidebar-filters'}>
-                <div className="sidebar-filter">O</div>
-                <div className="sidebar-filter">O</div>
-                <div className="sidebar-filter">O</div>
-                <div className="sidebar-filter">O</div>
+                <div className="sidebar-filter">
+                    <div className="sidebar-filter-icon">0</div>
+                    <div className="sidebar-filter-text">Description</div>
+                </div>
+                <div className="sidebar-filter">
+                    <div className="sidebar-filter-icon">0</div>
+                    <div className="sidebar-filter-text">Description</div>
+                </div>
+                <div className="sidebar-filter">
+                    <div className="sidebar-filter-icon">0</div>
+                    <div className="sidebar-filter-text">Description</div>
+                </div>
+                <div className="sidebar-filter">
+                    <div className="sidebar-filter-icon">0</div>
+                    <div className="sidebar-filter-text">Description</div>
+                </div>
             </div>
         </aside>
     )
